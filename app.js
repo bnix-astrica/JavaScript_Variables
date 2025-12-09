742c558af3a3f62ce6e4ee8be37d0369e8dd5336
Bruce Nix <bnix@astrica.ai>
2025-12-09 12:52:36 -0600

    Commit updates for excercises

--- app.js
+++ app.js
@@ -4,7 +4,49 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Bruce";
+lastName = "Nix";
+age = 59;
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age); 
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true; 
+console.log("Language:", language);
+console.log("Created Year:", createdYear);
+console.log("Is Case Sensitive:", isCaseSensitive);
+
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log("Price:", price);
+console.log("Is On Sale:", isOnSale);
+console.log("Sale Percentage:", salePercentage);
+console.log("Stock:", stock);
+console.log("In Stock:", inStock);
+console.log("Selected Size:", selectedSize);
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log("Title:", title);
+console.log("Author:", author);
+console.log("Page Count:", pageCount);
+console.log("Bookmark:", bookmark);
+console.log("Has Read:", hasRead);
